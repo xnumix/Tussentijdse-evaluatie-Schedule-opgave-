2870c0a59f1680298cf7168c90f0ab2b336980e2
xnumix <231570876+xnumix@users.noreply.github.com>
2026-05-03 21:47:50 +0200

    Create the DTO's to replace the lesson, break and excursion model. Creating a second constructor for break, that creates a break with only one argument

---
+++ Schedule.Persistance/DTO/DtoBreak.cs
@@ -0,0 +1,10 @@
+﻿namespace Schedule.Persistance.DTO
+{
+    internal class DtoBreak
+    {
+        TimeOnly StartTime { get; set; }
+        TimeOnly endTime { get; set; }
+        int DurationInMinutes { get; set; }
+        string catergory { get; set; }
+    }
+}
---
+++ Schedule.Persistance/Overig/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.Persistance.Overig
+{
+    internal class DbInitializer
+    {
+    }
+}
